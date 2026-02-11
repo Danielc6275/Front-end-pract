0cd7ccc31626aaf523e6a1aef91a4aaf53bc061b
danielc6275 <danielc6275@gmail.com>
2026-02-11 16:23:51 -0500

    .

---
+++ JSchallenges/estate/properties/placeholderPropertyObj.js
@@ -0,0 +1,7 @@
+export const placeholderPropertyObj = {
+  location: "Unknown",
+  price: 0,
+  rooms: [0],
+  comment: "No property data available",
+  image: "images/property1.jpg"
+}
---
+++ JSchallenges/estate/properties/propertyForSaleArr.js
@@ -0,0 +1,23 @@
+export const propertyForSaleArr = [
+  {
+    location: "Cape Town",
+    price: 1200000,
+    rooms: [2, 1, 1],
+    comment: "Beautiful ocean view apartment",
+    image: "images/property1.jpg"
+  },
+  {
+    location: "Johannesburg",
+    price: 950000,
+    rooms: [1, 1, 1],
+    comment: "Modern city living",
+    image: "images/property2.jpg"
+  },
+  {
+    location: "Pretoria",
+    price: 1500000,
+    rooms: [3, 2, 2],
+    comment: "Spacious family home",
+    image: "images/property3.jpg"
+  }
+]
